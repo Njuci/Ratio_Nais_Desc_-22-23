36cfd1792b3beca52ac73e049c3869944f789011
Njuci <augustinnjuci@gmail.com>
2023-09-09 13:16:51 +0200

    urlssettings

--- Ratio_Naiss_Desc/settings.py
+++ Ratio_Naiss_Desc/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'User'
+    'rest_framework',
+    
+    'User',
 ]
 
 MIDDLEWARE = [
