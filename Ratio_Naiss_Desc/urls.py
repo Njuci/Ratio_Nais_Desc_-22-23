8d8781525262c972234cf9916c254a52cedb71f8
Njuci <augustinnjuci@gmail.com>
2023-09-23 00:44:47 +0200

    'deploy settings 2'

--- Ratio_Naiss_Desc/urls.py
+++ Ratio_Naiss_Desc/urls.py
@@ -1,4 +1,5 @@
-
+from django.conf.urls.static import static
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path,include
 urlpatterns = [
@@ -6,3 +7,5 @@ urlpatterns = [
     path('user/',include('User.urls')),
     path('app/',include('V_H_C_A.urls'))
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
