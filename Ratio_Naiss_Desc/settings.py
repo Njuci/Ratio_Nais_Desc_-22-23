653a2d066e0bbead3d9b94420dc5020d654c4193
Njuci <augustinnjuci@gmail.com>
2023-09-23 01:27:36 +0200

    'essaie deploiement'

--- Ratio_Naiss_Desc/settings.py
+++ Ratio_Naiss_Desc/settings.py
@@ -68,7 +68,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'Ratio_Naiss_Desc.wsgi.application'
 
-
+import dj_database_url
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
@@ -83,7 +83,10 @@ DATABASES = {
         'PORT': '3306',
     }
 }
-
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+DATABASES['default']['OPTIONS']['charset'] = 'utf8mb4'
+del DATABASES['default']['OPTIONS']['sslmode'] 
+DATABASES['default']['OPTIONS']['ssl'] =  {'ca': os.environ.get('MYSQL_ATTR_SSL_CA')}
 
 '''
 DATABASES = {
