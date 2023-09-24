2eb30edc1a31363ddb83e66c5b798912beb2f8ed
Njuci <augustinnjuci@gmail.com>
2023-09-24 22:56:16 +0200

    'Filter certN'

--- V_H_C_A/urls.py
+++ V_H_C_A/urls.py
@@ -5,6 +5,7 @@ from .views import *
 urlpatterns = [
     path('create_province',CreateProvince.as_view(),name="create_province"),
     path('create_ville_or_Terri',CreateVilleTerr.as_view(),name='create_ville_or_Terri'),
-    path('create_certinaiss',Create_certificatNais.as_view(),name='create_certinaiss')
-    
+    path('create_certinaiss',Create_certificatNais.as_view(),name='create_certinaiss'),
+    path('get_cn_per_hosp',Get_CertN_par_hopital.as_view(),name='get_cn_per_hosp')
+
 ]
--- V_H_C_A/views.py
+++ V_H_C_A/views.py
@@ -114,31 +114,27 @@ class Create_ActeNais(APIView):
         
 
     
-    class Get_Cert_par_hopital(APIView):
-        def post(self,request):
-            token=request.data.get('token')
-            secret_key=settings.SECRET_KEY
-            verification_token=is_access_token_valid(token,secret_key)
-            user_type_authorized='hospital'
-            if verification_token[0]:
-                user=MyUser.objects.get(id=verification_token[1]['user_id'])
-                try:
-                    hptl=Hopital.objects.get(user=user.id)
-                except Hopital.DoesNotExist:
-                    hptl=None
-                if is_user_authorized(user.user_type,user_type_authorized):
-                    cert_par_hopital= Hopital.objects.filter(hospital_id=hptl.id).order_by('date_deliv_cert')
-                    serial=CertiNaissSerial(cert_par_hopital,many=True)
-                    return Response(serial.data,status=status.HTTP_200_OK)               
-                    
-                else:
-                    return Response({"message":"type d'utilisateur non autorisé"},status=status.HTTP_401_UNAUTHORIZED)
-                
+class Get_CertN_par_hopital(APIView):
+    def post(self,request):
+        token=request.data.get('token')
+        secret_key=settings.SECRET_KEY
+        verification_token=is_access_token_valid(token,secret_key)
+        user_type_authorized='hopital'
+        if verification_token[0]:
+            user=MyUser.objects.get(id=verification_token[1]['user_id'])
+            try:
+                hptl=Hopital.objects.get(user=user.id)
+            except Hopital.DoesNotExist:
+                hptl=None
+            if is_user_authorized(user.user_type,user_type_authorized):
+                cert_par_hopital= CertificatNaissance.objects.filter(hospital_id=hptl.id).order_by('date_deliv_cert')
+                serial=CertiNaissSerial(cert_par_hopital,many=True)
+                return Response(serial.data,status=status.HTTP_200_OK)               
+                   
             else:
-                return Response({"message":"utilisateur non autorisé"},status=status.HTTP_401_UNAUTHORIZED)
+                return Response({"message":"type d'utilisateur non autorisé"},status=status.HTTP_401_UNAUTHORIZED)
+                
+        else:
+            return Response({"message":"utilisateur non autorisé"},status=status.HTTP_401_UNAUTHORIZED)
             
             
-            cert=CertificatNaissance.objects.all().order_by('id')
-            serial=CertiNaissSerial(cert,many=True)
-               
-            return Response(serial.data,status=status.HTTP_200_OK)
