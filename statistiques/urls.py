c1275a9c735244813a1ba50deec567fa11e33284
Njuci <augustinnjuci@gmail.com>
2023-09-27 19:27:01 +0200

    ''statistiques certin Tous hop

--- statistiques/urls.py
+++ statistiques/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('voir_cert',Voir_stat.as_view(),name='voir_cert')
+    path('voir_cert',Voir_stat.as_view(),name='voir_cert'),
+    path('voir_cert_par_hop/<int:id>',Voir_stat_par_hop.as_view(),name='voir_cert_par_hop'),
 ]
--- statistiques/views.py
+++ statistiques/views.py
@@ -15,16 +15,14 @@ from itertools import groupby
 from django.db.models import Count
 
 class Voir_stat(APIView):
-    # Utilisez la méthode "annotate" sur le modèle "CertificatNaissance" pour obtenir le nombre de certificats en fonction des dates et du sexe
-    def get(self,request):
+    def get(self,request):      
         
-        certificats_par_date_et_sexe = CertificatNaissance.objects.values('date_deliv_cert', 'sexe_enfant').annotate(count=Count('id'))
         stat=[]
         # Parcourez les résultats pour afficher le nombre de certificats enregistrés pour chaque date et sexe
         # Obtenez tous les certificats groupés par date, sexe et hôpital
         try:
                 
-            certificats = CertificatNaissance.objects.values('date_deliv_cert', 'sexe_enfant', 'hospital_id').annotate(count=Count('id'))
+            certificats =certificats_par_date_et_sexe = CertificatNaissance.objects.values('date_deliv_cert', 'sexe_enfant').annotate(count=Count('id'))
         except CertificatNaissance.DoesNotExist:
             return Response({"message":"l'hopital n'a pas de certoificats enregistrer"})
             # Triez les certificats par date
@@ -54,13 +52,14 @@ class Voir_stat(APIView):
                 stata['date']=date_deliv_cert
                 stata['garçon']=nombre_garcons
                 stata['fille']=nombre_filles
-                stat.append(stata)
+                stata['total']=stata['fille']+stata['garçon']
+            stat.append(stata)
         
         return Response(stat,status=status.HTTP_200_OK)
     
 class Voir_stat_par_hop(APIView):
     # Utilisez la méthode "annotate" sur le modèle "CertificatNaissance" pour obtenir le nombre de certificats en fonction des dates et du sexe
-    def get(self,request):
+    def get(self,request,id):
         
         certificats_par_date_et_sexe = CertificatNaissance.objects.values('date_deliv_cert', 'sexe_enfant').annotate(count=Count('id'))
         stat=[]
@@ -68,7 +67,7 @@ class Voir_stat_par_hop(APIView):
         # Obtenez tous les certificats groupés par date, sexe et hôpital
         try:
                 
-            certificats = CertificatNaissance.objects.values('date_deliv_cert', 'sexe_enfant', 'hospital_id').filter(hospital_id=9).annotate(count=Count('id'))
+            certificats = CertificatNaissance.objects.values('date_deliv_cert', 'sexe_enfant', 'hospital_id').filter(hospital_id=id).annotate(count=Count('id'))
         except CertificatNaissance.DoesNotExist:
             return Response({"message":"l'hopital n'a pas de certoificats enregistrer"})
             # Triez les certificats par date
@@ -88,7 +87,7 @@ class Voir_stat_par_hop(APIView):
             for certificat in groupe:
                 sexe_enfant = certificat['sexe_enfant']
                 nombre_certificats = certificat['count']
-                print(sexe_enfant,nombre_certificats)
+                
                     
                 if sexe_enfant == 'f':
                     nombre_filles += nombre_certificats
@@ -98,6 +97,8 @@ class Voir_stat_par_hop(APIView):
                 stata['date']=date_deliv_cert
                 stata['garçon']=nombre_garcons
                 stata['fille']=nombre_filles
-                stat.append(stata)
+                stata['total']=stata['fille']+stata['garçon']
+            stat.append(stata)
+            
         
         return Response(stat,status=status.HTTP_200_OK)
