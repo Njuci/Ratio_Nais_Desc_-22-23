00a6cd9fc741cebc92e020482aa82a0b93eb51cb
Njuci <augustinnjuci@gmail.com>
2023-10-02 00:52:08 +0200

    essaie 51

--- Ratio_Naiss_Desc/wsgi.py
+++ Ratio_Naiss_Desc/wsgi.py
@@ -1,5 +1,5 @@
 import os
 from django.core.wsgi import get_wsgi_application
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Ratio_Nais_Desc.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Ratio_Naiss_Desc.settings")
 application = get_wsgi_application()
 app = application
